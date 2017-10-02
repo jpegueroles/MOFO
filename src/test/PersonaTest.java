b220d9aaa182e4560f51c66862a73f8aeccc1579
Jaime Pegueroles <jaime.pegueroles@gmail.com>
2017-10-02 17:32:17 -0300

    -add Test for Persona class.

---
+++ src/test/PersonaTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.Test;
+
+public class PersonaTest {
+    @Test
+    public void getNombre() throws Exception {
+    }
+
+    @Test
+    public void setNombre() throws Exception {
+    }
+
+    @Test
+    public void getApellido() throws Exception {
+    }
+
+    @Test
+    public void setApellido() throws Exception {
+    }
+
+    @Test
+    public void getEdad() throws Exception {
+    }
+
+    @Test
+    public void setEdad() throws Exception {
+    }
+
+    @Test
+    public void setInfo() throws Exception {
+    }
+
+}
